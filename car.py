1d5a9a382129dc705b4701a963d68f1e0477528d
Arogya Kharel <25946015+gurkhaman@users.noreply.github.com>
2025-05-19 03:57:55 +0000

    Add source code

---
+++ car.py
@@ -0,0 +1,29 @@
+class Car:
+   def __init__(self, speed=0):
+       self.speed = speed
+       self.odometer = 0
+       self.time = 0
+
+   def change_speed(self, change):
+       if change < 0:
+           if self.speed + change < 0:
+               self.speed = 0
+           else:
+               self.speed += change
+       else:
+           self.speed += change
+
+       self.step()
+
+   def get_current_speed(self):
+       return self.speed
+
+   def step(self):
+       self.odometer += self.speed
+       self.time += 1
+
+   def average_speed(self):
+       if self.time == 0:
+           raise Exception("Divide by 0! Car did not move!")
+       else:
+           return self.odometer / self.time
