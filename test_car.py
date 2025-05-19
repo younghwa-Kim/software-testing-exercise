eb296f37ca97469f3d8cacfcf25e37c3157e5257
Arogya Kharel <25946015+gurkhaman@users.noreply.github.com>
2025-05-19 04:01:28 +0000

    Formatting changes

--- test_car.py
+++ test_car.py
@@ -2,23 +2,29 @@ import pytest
 import random
 from car import Car
 
+
 @pytest.fixture
 def car():
     return Car()
 
+
 def test_initial_speed(car):
     assert car.speed == 0
+
 
 def test_initial_odometer(car):
     assert car.odometer == 0
 
+
 def test_initial_time(car):
     assert car.time == 0
+
 
 # Tests for changing speed
 def test_change_speed(car):
     car.change_speed(7)
     assert car.speed == 7
+
 
 def test_multiple_speed_change(car):
     total_change = 0
@@ -29,9 +35,11 @@ def test_multiple_speed_change(car):
         car.change_speed(change)
     assert car.speed == total_change
 
+
 # Tests for getting current speed
 def test_get_current_speed_from_start(car):
     assert car.get_current_speed() == 0
+
 
 def test_get_current_speed_after_change(car):
     car.change_speed(27)
